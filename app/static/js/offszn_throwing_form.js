a658591b5c4e5a6009d07007dffae2c768bfef11
jloeschorn5 <60281126+jloeschorn5@users.noreply.github.com>
2026-03-01 11:51:57 -0500

    added delete drill row to throwing plan

--- app/static/js/offszn_throwing_form.js
+++ app/static/js/offszn_throwing_form.js
@@ -1,8 +1,8 @@
 function addDrillRow() {
-    const container = document.getElementById('drills-container');
-    const newRow = document.createElement('div');
-    newRow.classList.add('drill-row');
-    newRow.innerHTML = `
+  const container = document.getElementById('drills-container');
+  const newRow = document.createElement('div');
+  newRow.classList.add('drill-row');
+  newRow.innerHTML = `
       <input type="text" name="drill_name[]" placeholder="Drill Name">
       <select name="drill_ball_weight[]">
         <option value="">Ball Weight</option>
@@ -23,16 +23,16 @@ function addDrillRow() {
       <input type="number" step="0.01" name="drill_velocity[]" placeholder="Max Velocity">
       <input type="number" step="0.01" name="throw_count[]" placeholder="Throw Count">
     `;
-    container.appendChild(newRow);
+  container.appendChild(newRow);
 }
 
 function deleteDrillRow() {
-    const container = document.getElementById('drills-container'); //parent element of all the drill rows
-    const divElements = container.querySelectorAll(".drill-row"); //divs in which drill rows live
-    const rowAmount = divElements.length;
-    const indextoDelete = rowAmount - 1;
+  const container = document.getElementById('drills-container'); //parent element of all the drill rows
+  const divElements = container.querySelectorAll(".drill-row"); //divs in which drill rows live
+  const rowAmount = divElements.length;
+  const indextoDelete = rowAmount - 1;
 
-    divElements[indextoDelete].remove();
+  divElements[indextoDelete].remove();
 
 
 }
